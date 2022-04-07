4cbda05b1054a87a7e42f2b47a0124569b749ae2
Philip Ciunkiewicz <philip.ciunkiewicz@ucalgary.ca>
2022-04-06 22:41:48 -0600

    clean up old files

--- src/bulk-stats.sh
+++
@@ -1,6 +0,0 @@
-#!\bin\bash
-
-for path in data/outputs-*; do
-    echo $path
-    /home/pciunkiewicz/miniconda3/envs/covid/bin/python export.py stats2 data/scenarios/eng301.json $path --flat=1 &> /dev/null;
-done
--- src/multirun.sh
+++
@@ -1,6 +0,0 @@
-#!\bin\bash
-
-for i in `seq 0 1000 9000`; do
-    # sync && echo 3 > /proc/sys/vm/drop_caches
-    /home/pciunkiewicz/miniconda3/envs/covid/bin/python launch.py run-parallel data/scenarios/eng301.json -n 6 -r 1000 -o $i;
-done
